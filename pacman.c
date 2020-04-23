fd7e3e62bfbbcc8ea6d6da18b75c6224f910a47a
Aditya Arole <aarole@purdue.edu>
2020-04-23 13:42:34 -0400

    testing new map

--- pacman.c
+++ pacman.c
@@ -4,7 +4,7 @@
 #include<math.h>
 #include<time.h>
 
-#define SIZE 15
+#define SIZE 30
 #define TRUE 1
 
 char get_input();
@@ -12,9 +12,11 @@ void print_map(char [SIZE][SIZE], int );
 int is_valid(char [SIZE][SIZE], int , int );
 int move(char [SIZE][SIZE], int , int , int );
 int generate_point(char [SIZE][SIZE]);
+void generate_ghost(char [SIZE][SIZE], int );
 int player_caught(char [SIZE][SIZE], int , int );
 void get_player_loc(char [SIZE][SIZE], int [2]);
 int ghost_valid(char [SIZE][SIZE], int , int );
+void alt_move(char [SIZE][SIZE], int , int );
 void move_ghost(char [SIZE][SIZE], int , int );
 int ghost_down(char [SIZE][SIZE], int , int );
 int ghost_up(char [SIZE][SIZE], int , int );
@@ -40,6 +42,7 @@ char get_input(){
 void print_map(char lcl_map[SIZE][SIZE], int pts){
 	system("clear");
 	generate_point(lcl_map);
+	generate_ghost(lcl_map,pts);
 	printf("	Points: %d\n\n", pts);
 
 	for(int i = 0; i < SIZE; i++){
@@ -132,6 +135,49 @@ int generate_point(char lcl_map[SIZE][SIZE]){
 	}
 }
 
+void generate_ghost(char lcl_map[SIZE][SIZE], int points){
+	int count = 0;
+	int p_row = 0;
+	int p_col = 0;
+
+	for(int i = 0; i < SIZE; i++){
+		for(int j = 0; j < SIZE; j++){
+			if(lcl_map[i][j] == 'P'){
+				p_row = i;
+				p_col = j;
+				break;
+			}
+		}
+		if(p_row != 0 && p_col != 0){
+			break;
+		}
+	}
+	for(int k = 0; k < SIZE; k++){
+		for(int l = 0; l < SIZE; l++){
+			if(lcl_map[k][l] == 'G'){
+				count++;
+			}
+		}
+	}
+
+	int row = 0;
+	int col = 0;
+
+	if(((points > 3 && points <= 6) && count == 2) || ((points > 6) && count == 3)){
+		while(TRUE){
+			row = (rand() % (SIZE + 1));
+			col = (rand() % (SIZE + 1));
+			int x_rad = abs((row-p_row));
+			int y_rad = abs((col-p_col));
+
+			if(lcl_map[row][col] != 'P' && lcl_map[row][col] != 'G' && lcl_map[row][col] != '#' && lcl_map[row][col] != 'o' && x_rad > 5 && y_rad > 5){
+				lcl_map[row][col] = 'G';
+				break;
+			}
+		}
+	}
+}
+
 int player_caught(char lcl_map[SIZE][SIZE], int r, int c){
 	if(lcl_map[r+1][c] == 'G' || 
 	lcl_map[r-1][c] == 'G' ||
@@ -160,7 +206,7 @@ void get_player_loc(char lcl_map[SIZE][SIZE], int temp[2]){
 }
 
 int ghost_valid(char lcl_map[SIZE][SIZE], int r, int c){
-	if(r>=0 && r<SIZE && c>=0 && c<SIZE && (lcl_map[r][c] == 'o' || lcl_map[r][c] == ' ' || lcl_map[r][c] == 'G')){
+	if(r>=0 && r<SIZE && c>=0 && c<SIZE && (lcl_map[r][c] == 'o' || lcl_map[r][c] == ' ')){
 		return 1;
 	}
 
@@ -168,18 +214,63 @@ int ghost_valid(char lcl_map[SIZE][SIZE], int r, int c){
 }
 
 char prev = 'o';
-void move_ghost(char lcl_map[SIZE][SIZE], int r, int c){
-	if(prev == 'w'){
-		if(lcl_map[r-1][c] == '#'){
-			prev = 'o';
+void alt_move(char lcl_map[SIZE][SIZE], int r, int c){
+	//Top and left
+	if(lcl_map[r-1][c] == '#' && lcl_map[r][c-1] == '#'){
+		int pick = rand() % 2;
+		if(pick){
+			ghost_right(lcl_map, r, c);
+			prev = 'd';
 		}
 		else{
-			ghost_up(lcl_map,r,c);
+			ghost_down(lcl_map, r, c);
+			prev = 's';
 		}
 	}
-	else if(prev == 's'){
+	//Top and right
+	else if(lcl_map[r-1][c] == '#' && lcl_map[r][c+1] == '#'){
+		int pick = rand() % 2;
+		if(pick){
+			ghost_left(lcl_map, r, c);
+			prev = 'a';
+		}
+		else{
+			ghost_down(lcl_map, r, c);
+			prev = 's';
+		}
+	}
+	//Bottom and left
+	else if(lcl_map[r+1][c] == '#' && lcl_map[r][c-1] == '#'){
+		int pick = rand() % 2;
+		if(pick){
+			ghost_right(lcl_map, r, c);
+			prev = 'd';
+		}
+		else{
+			ghost_up(lcl_map, r, c);
+			prev = 'w';
+		}
+	}
+	//Bottom and right
+	else{
+		int pick = rand() % 2;
+		if(pick){
+			ghost_left(lcl_map, r, c);
+			prev = 'a';
+		}
+		else{
+			ghost_up(lcl_map, r, c);
+			prev = 'w';
+		}
+	}
+}
+
+void move_ghost(char lcl_map[SIZE][SIZE], int r, int c){
+	
+	if(prev == 's'){
 		if(lcl_map[r+1][c] == '#'){
 			prev = 'o';
+			alt_move(lcl_map,r,c);
 		}
 		else{
 			ghost_down(lcl_map,r,c);
@@ -188,6 +279,7 @@ void move_ghost(char lcl_map[SIZE][SIZE], int r, int c){
 	else if(prev == 'a'){
 		if(lcl_map[r][c-1] == '#'){
 			prev = 'o';
+			alt_move(lcl_map,r,c);
 		}
 		else{
 			ghost_left(lcl_map,r,c);
@@ -196,64 +288,26 @@ void move_ghost(char lcl_map[SIZE][SIZE], int r, int c){
 	else if(prev == 'd'){
 		if(lcl_map[r][c+1] == '#'){
 			prev = 'o';
+			alt_move(lcl_map,r,c);
 		}
 		else{
 			ghost_right(lcl_map,r,c);
 		}
 	}
-	else{
-		//Top and left
-		if(lcl_map[r-1][c] == '#' && lcl_map[r][c-1] == '#'){
-			int pick = rand() % 2;
-			if(pick){
-				ghost_right(lcl_map, r, c);
-				prev = 'd';
-			}
-			else{
-				ghost_down(lcl_map, r, c);
-				prev = 's';
-			}
+	else if(prev == 'w'){
+		if(lcl_map[r-1][c] == '#'){
+			prev = 'o';
+			alt_move(lcl_map,r,c);
 		}
-		//Top and right
-		else if(lcl_map[r-1][c] == '#' && lcl_map[r][c+1] == '#'){
-			int pick = rand() % 2;
-			if(pick){
-				ghost_left(lcl_map, r, c);
-				prev = 'a';
-			}
-			else{
-				ghost_down(lcl_map, r, c);
-				prev = 's';
-			}
-		}
-		//Bottom and left
-		else if(lcl_map[r+1][c] == '#' && lcl_map[r][c-1] == '#'){
-			int pick = rand() % 2;
-			if(pick){
-				ghost_right(lcl_map, r, c);
-				prev = 'd';
-			}
-			else{
-				ghost_up(lcl_map, r, c);
-				prev = 'w';
-			}
-		}
-		//Bottom and right
 		else{
-			int pick = rand() % 2;
-			if(pick){
-				ghost_left(lcl_map, r, c);
-				prev = 'a';
-			}
-			else{
-				ghost_up(lcl_map, r, c);
-				prev = 'w';
-			}
+			ghost_up(lcl_map,r,c);
 		}
+	}
+	else{
+		
 	}
 	
 }
-
 
 int ghost_down(char lcl_map[SIZE][SIZE], int r, int c){
 	if(ghost_valid(lcl_map, r+1, c)){
@@ -268,6 +322,9 @@ int ghost_down(char lcl_map[SIZE][SIZE], int r, int c){
 		}
 		else{
 			r += 1;
+		}
+		if(lcl_map[r][c] == 'G'){
+			return 0;
 		}
 		lcl_map[r][c] = 'G';
 	}
@@ -287,6 +344,9 @@ int ghost_up(char lcl_map[SIZE][SIZE], int r, int c){
 		else{
 			r -= 1;
 		}
+		if(lcl_map[r][c] == 'G'){
+			return 0;
+		}
 		lcl_map[r][c] = 'G';
 	}
 }
@@ -304,6 +364,9 @@ int ghost_left(char lcl_map[SIZE][SIZE], int r, int c){
 		}
 		else{
 			c -= 1;
+		}
+		if(lcl_map[r][c] == 'G'){
+			return 0;
 		}
 		lcl_map[r][c] = 'G';
 	}
@@ -323,6 +386,9 @@ int ghost_right(char lcl_map[SIZE][SIZE], int r, int c){
 		}
 		else{
 			c += 1;
+		}
+		if(lcl_map[r][c] == 'G'){
+			return 0;
 		}
 		lcl_map[r][c] = 'G';
 	}
@@ -350,22 +416,39 @@ int main(){
 
 	while(TRUE){
 		system("clear");
+		//New row: {'#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#'}
+		//New col: ,' ',' ',' ',' ','#'
 
-		char map[SIZE][SIZE] = { {'#','#','#','#','#','#','#','#','#','#','#','#','#','#','#'},
-								 {'#',' ',' ',' ','#',' ',' ',' ',' ',' ',' ',' ',' ',' ','#'},
-								 {'#',' ',' ',' ','#','#',' ',' ',' ',' ',' ',' ',' ',' ','#'},
-								 {'#',' ',' ',' ',' ','#',' ',' ','#','#',' ',' ',' ',' ','#'},
-								 {'#',' ','#',' ',' ','#',' ',' ',' ','#',' ',' ',' ',' ','#'},
-								 {'#',' ','#',' ','#','#',' ',' ',' ','#',' ',' ',' ',' ','#'},
-								 {'#',' ','#',' ','#','P',' ',' ',' ','#',' ',' ',' ',' ','#'},
-								 {'#','G',' ',' ',' ',' ',' ',' ',' ','#','#','#',' ',' ','#'},
-								 {'#',' ',' ','#',' ',' ',' ',' ',' ',' ',' ','#',' ',' ','#'},
-								 {'#','#','#','#',' ',' ',' ','#','#',' ',' ','#',' ',' ','#'},
-								 {'#',' ',' ',' ',' ',' ',' ','#',' ',' ',' ',' ',' ',' ','#'},
-								 {'#',' ',' ',' ','#','#','#','#',' ',' ',' ',' ',' ',' ','#'},
-								 {'#',' ',' ',' ','#',' ',' ',' ',' ',' ','#',' ',' ',' ','#'},
-								 {'#',' ',' ',' ',' ',' ',' ',' ',' ',' ','#',' ',' ','G','#'},
-								 {'#','#','#','#','#','#','#','#','#','#','#','#','#','#','#'}};
+		char map[SIZE][SIZE] = { {'#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#'},
+								 {'#','G',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+								 {'#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+								 {'#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+								 {'#',' ',' ','#','#','#','#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#','#','#','#',' ',' ','#'},
+								 {'#',' ',' ','#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#',' ',' ','#'},
+								 {'#',' ',' ','#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#',' ',' ','#'},
+								 {'#',' ',' ','#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#',' ',' ','#'},
+								 {'#',' ',' ','#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#',' ',' ','#'},
+								 {'#',' ',' ','#',' ','#','#','#','#','#','#','#',' ',' ',' ',' ',' ',' ','#','#','#','#','#','#','#',' ','#',' ',' ','#'},
+								 {'#',' ',' ','#',' ','#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#',' ','#',' ',' ','#'},
+								 {'#',' ',' ',' ',' ','#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#',' ',' ',' ',' ','#'},
+								 {'#',' ',' ',' ',' ','#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#',' ',' ',' ',' ','#'},
+								 {'#',' ',' ',' ',' ','#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#',' ',' ',' ',' ','#'},
+								 {'#',' ',' ',' ',' ',' ',' ',' ',' ',' ','#','#','#','#',' ',' ','#','#','#','#',' ',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+								 {'#',' ',' ',' ',' ',' ',' ',' ',' ',' ','#',' ',' ',' ',' ',' ',' ',' ',' ','#',' ',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+								 {'#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+								 {'#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','P',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+								 {'#',' ',' ',' ',' ',' ',' ',' ',' ',' ','#',' ',' ',' ',' ',' ',' ',' ',' ','#',' ',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+								 {'#',' ',' ',' ',' ',' ',' ',' ',' ',' ','#','#','#','#',' ',' ','#','#','#','#',' ',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+								 {'#',' ',' ',' ',' ','#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#',' ',' ',' ',' ','#'},
+								 {'#',' ',' ',' ',' ','#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#',' ',' ',' ',' ','#'},
+								 {'#',' ',' ',' ',' ','#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#',' ',' ',' ',' ','#'},
+								 {'#',' ',' ','#',' ','#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#',' ','#',' ',' ','#'},
+								 {'#',' ',' ','#',' ','#','#','#','#','#','#','#',' ',' ',' ',' ',' ',' ','#','#','#','#','#','#','#',' ','#',' ',' ','#'},
+								 {'#',' ',' ','#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#',' ',' ','#'},
+								 {'#',' ',' ','#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#',' ',' ','#'},
+								 {'#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+								 {'#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','G','#'},
+								 {'#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#'}};
 	
 		int points = 0;
 		int moves = 0;
@@ -383,6 +466,15 @@ int main(){
 		time_t start = time(NULL);
 
 		int caught = 0;
+		for(int a = 0; a < SIZE; a++){
+			for(int b = 0; b < SIZE; b++){
+				if(map[a][b] == 'G'){
+					int temp_a = a;
+					int temp_b = b;
+					alt_move(map, temp_a, temp_b);
+				}
+			}
+		}
 
 		while(points != 10){
 			char x = get_input();
