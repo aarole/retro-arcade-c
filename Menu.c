99106a3dbfede42d11786383e7bedbe3df9e488e
Taurus3301 <58012753+Taurus3301@users.noreply.github.com>
2020-05-02 18:52:17 -0400

    commented menu.c

--- Menu.c
+++ Menu.c
@@ -1,3 +1,4 @@
+//import games and header files
 #include "./pacman/pacman.c"
 #include "./tictactoe/tic-tac-toe.c"
 //#include "./snake/snake.c"
@@ -8,13 +9,17 @@
 
 int main()
 {
-    int count = 0;
-    while (1)
+    int count = 0;// count for no. of games played
+    while (1)// constantly loop until exit
     {
+	// clear terminal
         system("clear");
+	// get current date
         char today[10];
         get_date(today);
+	// create variable for input
         int game;
+	// display menu
         printf("\nWelcome to Retro Arcade\n");
         printf("Made by Aditya Arole, Resul Ucar, Will Grech, Will Woo, Zixiao Li\n");
         printf("__________________________________________________________________________________________\n");
@@ -28,38 +33,39 @@ int main()
         printf("5. Rock, Paper, Scissors\n");
         printf("Please enter the game number (0 to exit): ");
         scanf("%d", &game);
+	// read in input and call relevant game
         switch (game)
         {
-            case 1:
+            case 1:// snake game is not ready
             {
 		printf("Snake is not ready yet.\n");
                 //snake();
             }
-            case 2:
+            case 2:// pacman game
             {
                 count++;
                 pacman();
                 break;
             }
-            case 3:
+            case 3:// tictactoe game
             {
                 count++;
                 tictactoe();
                 break;
             }
-            case 4:
+            case 4:// minesweeper game
             {
                 count++;
                 minesweeper();
                 break;
             }
-            case 5:
+            case 5:// rock paper scissors game
             {
                 count++;
                 rockpaperscissors();
                 break;
             }
-            default:
+            default:// if not any of the previous selections, exit
             {
                 printf("\nThanks for playing! Good bye!\n");
                 exit(0);
